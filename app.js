b32980a780346fbd359c37436081430ea18026e4
Eduardo San Martin Morote <posva13@gmail.com>
2017-08-10 22:56:29 +0200

    Add signin route with jwt

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 
+const passport = require("passport");
+const User = require("./models/user");
+const config = require("./config");
+const { Strategy, ExtractJwt } = require("passport-jwt");
+
 mongoose.connect("mongodb://localhost/blog-lab", { useMongoClient: true });
 
 const app = express();
@@ -14,6 +19,34 @@ const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+passport.initialize();
+// Create the strategy for JWT
+const strategy = new Strategy(
+  {
+    // this is a config we pass to the strategy
+    // it needs to secret to decrypt the payload of the
+    // token.
+    secretOrKey: config.jwtSecret,
+    // This options tells the strategy to extract the token
+    // from the header of the request
+    jwtFromRequest: ExtractJwt.fromAuthHeader()
+  },
+  (payload, done) => {
+    // payload is the object we encrypted at the route /api/token
+    // We get the user id, make sure the user exist by looking it up
+    User.findById(payload.id).then(user => {
+      if (user) {
+        // make the user accessible in req.user
+        done(null, user);
+      } else {
+        done(new Error("User not found"));
+      }
+    });
+  }
+);
+// tell pasport to use it
+passport.use(strategy);
 
 const index = require('./routes/index');
 const authRoutes = require("./routes/auth");
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const jwt = require("jwt-simple");
 const router = express.Router();
 const User = require('../models/user');
+const config = require("../config");
 
 router.post('/signup', (req, res, next) => {
   // extract the info we need from the body
@@ -26,6 +28,45 @@ router.post('/signup', (req, res, next) => {
     }
     res.json({ success: true })
   })
+});
+
+// User.authenticate() returns a function
+const authenticate = User.authenticate();
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  // check if we have a username and password
+  if (username && password) {
+    // test if the credentials are valid
+    authenticate(username, password, (err, user, failed) => {
+      if (err) {
+        // an unexpected error from the database
+        return res.status(500).json(err);
+      }
+      if (failed) {
+        // failed logging (bad password, too many attempts, etc)
+        return res.status(401).json(failed.message);
+      }
+      if (user) {
+        // success!! Save the user id
+        // NEVER save the password here
+        // the id is usually enough because we can get back
+        // the actual user by fetching the database later
+        const payload = {
+          id: user.id
+        };
+        // generate a token and send it
+        // this token will contain the user.id encrypted
+        // only the server is able to decrypt it
+        // for the client, this is just a token, he knows that
+        // he has to send it
+        const token = jwt.encode(payload, config.jwtSecret);
+        res.json({ token });
+      }
+    });
+  } else {
+    // unauthorized error
+    res.sendStatus(401);
+  }
 });
 
 module.exports = router;
